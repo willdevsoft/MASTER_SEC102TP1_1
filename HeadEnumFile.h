1ddd2d55278fe71d545c74ba2af15294afc8c4f9
willdevsoft <w_poisson@yahoo.fr>
2017-04-14 00:34:24 +0200

    Tentative rajout date création et modification

--- HeadEnumFile.h
+++ HeadEnumFile.h
@@ -8,3 +8,4 @@ Rôle : prototypes des fonctions de l'énumération de fichiers
 */
 
 void EnumFilesDirectory(CString strFilePath);
+BOOL GetLastWriteTime(HANDLE hFile, LPTSTR lpszString, DWORD dwSize);
--- Src_Enum_File.cpp
+++ Src_Enum_File.cpp
@@ -39,7 +39,12 @@ void EnumFilesDirectory(CString strFilePath)
 	DWORD dwFileSize;
 	DWORD dwFileType;
 	LARGE_INTEGER filesize;
-
+	FILETIME ftCreate, ftAccess, ftWrite;
+	SYSTEMTIME stUTC, stLocal;
+	DWORD dwRet;
+	LPTSTR lpszString;
+	DWORD dwSize;
+	TCHAR szBuf[MAX_PATH];
 	// Utilisation de l'API FinFirstFile et FindNextFile pour récupérer le handle de chaque fichier du répertoire
 
 	if ((hEnt = FindFirstFile(strFilePath, &ent)) != INVALID_HANDLE_VALUE)
@@ -52,11 +57,55 @@ void EnumFilesDirectory(CString strFilePath)
 			dwFileSize = GetFileSize(hEnt, NULL);
 			filesize.LowPart = ent.nFileSizeLow;
 			filesize.HighPart = ent.nFileSizeHigh;
+			// Récupére la date et l'heure de la création du fichier
+			BOOL WINAPI GetFileTime(
+				_In_      HANDLE     hEnt,
+				_Out_opt_ LPFILETIME lpCreationTime,
+				_Out_opt_ LPFILETIME lpLastAccessTime,
+				_Out_opt_ LPFILETIME lpLastWriteTime
+			);
+			GetFileTime(hEnt, &ftCreate, &ftAccess, &ftWrite);
+
+			// Convert the last-write time to local time.
+			FileTimeToSystemTime(&ftWrite, &stUTC);
+			SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
+
+			// Build a string showing the date and time.
+//			dwRet = StringCchPrintf(lpszString, dwSize,
+	//			TEXT("%02d/%02d/%d  %02d:%02d"),
+//				stLocal.wMonth, stLocal.wDay, stLocal.wYear,
+	//			stLocal.wHour, stLocal.wMinute);
+			_tprintf(TEXT("Mois: %02d "), stLocal.wMonth);
+		
+
 			// Affiche <Nom du fichier><Taille><type>
-			_tprintf(TEXT("hFile size: %10d\n"), filesize.HighPart);
 			_tprintf(TEXT("Nom du fichier: %s Taille:  %ld bytes type: %d\n"), ent.cFileName, filesize.QuadPart, dwFileType);
 		} while (FindNextFile(hEnt, &ent));
 
 		FindClose(hEnt);
 	}
+}
+BOOL GetLastWriteTime(HANDLE hFile, LPTSTR lpszString, DWORD dwSize)
+{
+	FILETIME ftCreate, ftAccess, ftWrite;
+	SYSTEMTIME stUTC, stLocal;
+	DWORD dwRet;
+
+	// Retrieve the file times for the file.
+	if (!GetFileTime(hFile, &ftCreate, &ftAccess, &ftWrite))
+		return FALSE;
+
+	// Convert the last-write time to local time.
+	FileTimeToSystemTime(&ftWrite, &stUTC);
+	SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
+
+	// Build a string showing the date and time.
+	dwRet = StringCchPrintf(lpszString, dwSize,
+		TEXT("%02d/%02d/%d  %02d:%02d"),
+		stLocal.wMonth, stLocal.wDay, stLocal.wYear,
+		stLocal.wHour, stLocal.wMinute);
+
+	if (S_OK == dwRet)
+		return TRUE;
+	else return FALSE;
 }
